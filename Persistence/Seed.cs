29a0c06012388b510f13be1f57e40ff7a65383b1
Nancy Aggarwal <tibatec_nancy@os4w.de>
2022-04-05 15:46:19 +0530

    end of session 2

---
+++ Persistence/Seed.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public  static async Task SeedData(DataContext context)
+        {
+            if (context.Activities.Any()) return;
+            var activities = new List<Activity>
+            {
+                new Activity
+                {
+                    Title= "Past Activity 1",
+                    Date = DateTime.Now.AddMonths(-2),
+                  //  Description= "Activity 2 months ago",
+                    Category= "drinks",
+                    City= "London",
+                    Venue= "Pub",
+                },
+                 new Activity
+                {
+                    Title= "Past Activity 2",
+                    Date = DateTime.Now.AddMonths(-2),
+                    //Description= "Activity 2 months ago",
+                    Category= "food",
+                    City= "London",
+                    Venue= "Hotel",
+                },
+                 new Activity
+                {
+                    Title= "Future Activity 3",
+                    Date = DateTime.Now.AddMonths(1),
+                   // Description= "Activity 1 months in future",
+                    Category= "film",
+                    City= "London",
+                    Venue= "Cinema",
+                },
+                 new Activity
+                {
+                    Title= "Past Activity 4",
+                    Date = DateTime.Now.AddMonths(2),
+                  //  Description= "Activity 2 months in future",
+                    Category= "Outing",
+                    City= "London",
+                    Venue= "Somewhere on hillstation",
+                },
+            };
+
+            await context.Activities.AddRangeAsync(activities);
+            await context.SaveChangesAsync();
+        }
+    }
+}
