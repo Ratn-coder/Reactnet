777218123b0791de27d6a1bbe7f7ba36d4b3599c
Nancy Aggarwal <tibatec_nancy@os4w.de>
2022-04-19 17:55:53 +0530

    Edit Part

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import axios, { Axios } from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
 
 function App() {
 
@@ -13,9 +13,13 @@ function App() {
   const [editMode, setEditMode] = useState(false);
   
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      console.log(response);
-      setActivities(response.data);
+  agent.Activities.list().then(response => {
+    let activities: Activity[] =[];
+    response.forEach(activity => {
+      activity.date = activity.date.split('T') [0];
+      activities.push(activity);
+    })
+      setActivities(activities);
     })
   }, [])
 
